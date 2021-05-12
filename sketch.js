a9567c344cc9f045de7978aaa88df3ccf71fc637
EkavirDubey <78644977+EkavirDubey@users.noreply.github.com>
2021-05-12 15:56:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var a,b 
+
+
+
+
+function setup() {
+  createCanvas(800,400);
+  a = createSprite(400, 200, 30, 50);
+  b = createSprite(300,200,50,50);
+
+}
+
+function draw() {
+  
+  background("yellow");  
+  console.log(a.x-b.x);
+  a.x = mouseX;
+  a.y = mouseY;
+ if(a.x-b.x<a.width/2 + b.width/2 && b.x-a.x<a.width/2 + b.width/2
+  && a.y-b.y<a.height/2 + b.height/2 && b.y-a.y<a.height/2 + b.height/2 ){
+  
+    a.shapeColor = "red";
+    b.shapeColor = "pink";
+  }
+  else {
+    a.shapeColor = "black";
+    b.shapeColor = "black";
+ } 
+
+  
+  drawSprites();
+}
